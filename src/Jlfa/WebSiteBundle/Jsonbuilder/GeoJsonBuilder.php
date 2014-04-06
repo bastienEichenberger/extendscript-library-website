f193f249c926e1d24da48f2443d0c9010782b96f
bastienEichenberger <bastieneichenberger@MacBook-Pro-de-bastien.local>
2014-04-06 15:52:47 +0200

    comments classes

--- src/Jlfa/WebSiteBundle/Jsonbuilder/GeoJsonBuilder.php
+++ src/Jlfa/WebSiteBundle/Jsonbuilder/GeoJsonBuilder.php
@@ -5,12 +5,39 @@ namespace Jlfa\WebSiteBundle\Jsonbuilder;
 use Sdz\UserBundle\Lib\Geo\Feature;
 use Sdz\UserBundle\Lib\Geo\FeatureCollection;
 
-class GeoJsonBuilder {
 
+/**
+ * GeooJsonBuilder, class to generate a geojson file
+ */
+class GeoJsonBuilder {
+    
+    /** 
+     * @var EntityManager $em
+     */
     private $em;
+    
+    /**
+     * 
+     * @var Router $router
+     */
     private $router;
+    
+    /**
+     * The max memory limit to build the geojson file
+     * @var $memory_limit_max 32M
+     */
     private $memory_limit_max;
+    
+    /**
+     * The standard memory limit
+     * @var $memory_limit_standard 
+     */
     private $memory_limit_standard;
+    
+    /**
+     *
+     * @var SQLLogger $logger_standard
+     */
     private $logger_standard;
 
     public function __construct(\Doctrine\ORM\EntityManager $em, \Symfony\Component\Routing\Router $router) {
@@ -20,26 +47,46 @@ class GeoJsonBuilder {
         $this->memory_limit_standard = ini_get('memory_limit');
         $this->logger_standard = $this->em->getConnection()->getConfiguration()->getSQLLogger();
     }
-
+    
+    
+    /**
+     * Function to begin big sql query (limit to max, and set the logger to null)
+     */
     private function begin_query() {
         ini_set('memory_limit', $this->memory_limit_max);
         $this->em->getConnection()->getConfiguration()->setSQLLogger(null);
     }
-
+    
+    /**
+     * Function to reset the standards params
+     */
     private function end_query() {
         ini_set('memory_limit', $this->memory_limit_standard);
         $this->em->getConnection()->getConfiguration()->setSQLLogger($this->logger_standard);
     }
     
-    
+    /**
+     * Function to return the root upload dir
+     * @return string rootUploadDir
+     */
     private function getUploadRootDir() {
         return __DIR__ . '/../' . $this->getUploadDir();
     }
-
+    
+    /**
+     * Function to return the upload dir
+     * @return string uploadDir
+     */
     private function getUploadDir() {
         return 'Resources/public/data';
     }
-
+    
+    /**
+     * Function to create a json file
+     * @param string $file_name
+     * @param array $data
+     * @param strig $content the content to add at the beginning of the file
+     */
     private function createJSONFile($file_name, $data, $content = null){
         $DATA_DIR = $this->getUploadRootDir();
         $file = fopen($DATA_DIR . '/' . $file_name . '.js', 'w');
@@ -47,7 +94,12 @@ class GeoJsonBuilder {
         fwrite($file, json_encode($data));
         fclose($file);
     }
-
+    
+    /**
+     * Function to execute sql custom query
+     * @param type $sql
+     * @return array $statement
+     */
     private function execute_statement($sql) {
         $this->begin_query();
         $connection = $this->em->getConnection();
@@ -56,7 +108,10 @@ class GeoJsonBuilder {
         $this->end_query();
         return $statement;
     }
-
+    
+    /**
+     * Function to generate the geojson file with all countries (density, name)
+     */
     public function updateCountryJSON() {
 
         $sql = "SELECT gid, name, ST_AsGeoJSON(world.geom) as geom, count(tut_user.location) AS density, 
@@ -68,7 +123,8 @@ class GeoJsonBuilder {
 
         $statement = $this->execute_statement($sql);
         $fc = new FeatureCollection();
-
+        
+        // build feature collection
         $total = null;
         while ($row = $statement->fetch()) {
 
@@ -87,7 +143,10 @@ class GeoJsonBuilder {
         
         $this->createJSONFile('countries', $fc, 'var countries =');
     }
-
+    
+    /**
+     * Function to update members json file get only members with displayadresse to true
+     */
     public function updateMembersJSON() {
 
         $sql = "SELECT id, username, aboutme, ST_AsGeoJSON(location) AS geom
--- src/Sdz/BlogBundle/Controller/BlogController.php
+++ src/Sdz/BlogBundle/Controller/BlogController.php
@@ -18,25 +18,25 @@ use Symfony\Component\Security\Acl\Domain\UserSecurityIdentity;
 use Symfony\Component\Security\Acl\Permission\MaskBuilder;
 use Symfony\Component\Security\Acl\Domain\RoleSecurityIdentity;
 
+
+/**
+ * Class to control the blog
+ */
 class BlogController extends Controller {
-
+    
+    
     public function indexAction($page) {
-
-
-
-
-        // On récupère le nombre d'article par page depuis un paramètre du conteneur
-        // cf app/config/parameters.yml
+        // articles per page
         $nbParPage = $this->container->getParameter('sdzblog.nombre_par_page');
         $em = $this->getDoctrine()->getManager();
-        // On récupère les articles de la page courante
+        
+        // get articles of current page
         $articles = $em->getRepository('SdzBlogBundle:Article')
                 ->getArticles($nbParPage, $page);
 
         $last_comments = $em->getRepository('SdzBlogBundle:Commentaire')
                 ->getFiveLastComments();
 
-        // On passe le tout à la vue
         return $this->render('SdzBlogBundle:blog:index.html.twig', array(
                     'articles' => $articles,
                     'last_comments' => $last_comments,
@@ -45,17 +45,27 @@ class BlogController extends Controller {
                     'nombre_sur_page' => $articles->getIterator()->count()
         ));
     }
-
+    
+    /**
+     * Function to generate delete form (used in delete comment and index views)
+     * @param integer $id the id of entity to delete
+     * @return TwigTemplate
+     */
     public function delete_formAction($id) {
 
         $deleteForm = $this->createDeleteForm($id);
 
         return $this->render('SdzBlogBundle:blog:delete_form.html.twig', array(
-                    'form' => $deleteForm->createView(),
-                        )
+                        'form' => $deleteForm->createView(),
+                    )
         );
     }
-
+    
+    /**
+     * Function to create the delete form
+     * @param integer $id
+     * @return Form deleteform
+     */
     protected function createDeleteForm($id) {
         return $this->createFormBuilder(array('id' => $id))
                         ->add('id', 'hidden')
@@ -63,7 +73,9 @@ class BlogController extends Controller {
     }
 
     /**
+     * Function to add a post
      * @Secure(roles="ROLE_AUTHOR")
+     * @return TwigTemplate
      */
     public function addPostAction() {
 
@@ -77,9 +89,7 @@ class BlogController extends Controller {
 
         if ($request->isMethod('POST')) {
             $form->bind($request);
-
             if ($form->isValid()) {
-
                 // persisting multiple translations, assume default locale is FR
                 $em = $this->getDoctrine()->getManager();
                 $article = $em->merge($article);
@@ -104,21 +114,17 @@ class BlogController extends Controller {
         ));
     }
 
+    
     /**
+     * Function to edit an article
+     * @param \Sdz\BlogBundle\Entity\Article $article
      * @Secure(roles="ROLE_ADMIN")
-     */
-    /**
-     * @Secure(roles="ROLE_ADMIN")
+     * @return TwigTemplate
      */
     public function editAction(Article $article) {
         
-       
-       
-
         $request = $this->getRequest();
         $em = $this->getDoctrine()->getManager();
-        
-        
         
         if ($request->getMethod() == 'POST') {
             $form = $this->createForm(new ArticleEditType(), $article);
@@ -127,7 +133,6 @@ class BlogController extends Controller {
             if ($form->isValid() ) {
                 $translatableListener = $this->get('stof_doctrine_extensions.listener.translatable');
                 $translatableListener->setTranslatableLocale($translatableListener->getDefaultLocale());
-                
                 
                 $repository = $em->getRepository('Gedmo\\Translatable\\Entity\\Translation');
                 $repository->translate($article, 'titre', 'de', $article->getTitreDe())
@@ -145,7 +150,8 @@ class BlogController extends Controller {
             }
         }
         
-         // On utilise le ArticleEditType
+        // this part have to been rewrite. Asumme we can use a query walker
+        // https://github.com/l3pp4rd/DoctrineExtensions/blob/master/doc/translatable.md#using-orm-query-hint
         $article->setTranslatableLocale('en');
         $em->refresh($article);
         $content_en = $article->getContenu();
@@ -171,10 +177,17 @@ class BlogController extends Controller {
                     'article' => $article
         ));
     }
-
+    
+    
+    /**
+     * Function to display an article
+     * @param \Sdz\BlogBundle\Entity\Article $article
+     * @param \Symfony\Component\Form\Form $form
+     * @return TwigTemplate
+     */
     public function voirAction(Article $article, Form $form = null) {
-        $em = $this->getDoctrine()->getManager();
 
+        $em = $this->getDoctrine()->getManager();
 
         $commentaires = $em->getRepository('SdzBlogBundle:Commentaire')
                 ->getByArticle($article->getId());
@@ -182,7 +195,6 @@ class BlogController extends Controller {
         if (null === $form) {
             $form = $this->getCommentaireForm($article);
         }
-
 
         return $this->render('SdzBlogBundle:blog:post.html.twig', array(
                     'article' => $article,
@@ -192,7 +204,10 @@ class BlogController extends Controller {
     }
 
     /**
+     * Function to delete an article
+     * @param \Sdz\BlogBundle\Entity\Article $article
      * @Secure(roles="ROLE_ADMIN")
+     * @return TwigTemplate
      */
     public function supprimerAction(Article $article) {
 
@@ -201,62 +216,62 @@ class BlogController extends Controller {
         if ($request->getMethod() == 'POST') {
             $form->bind($request);
 
-            if ($form->isValid()) { // Ici, isValid ne vérifie donc que le CSRF
-                // On supprime l'article
+            if ($form->isValid()) { 
                 $em = $this->getDoctrine()->getManager();
                 $em->remove($article);
                 $em->flush();
 
-                // On définit un message flash
                 $this->get('session')->getFlashBag()->add('info', 'Article bien supprimé');
 
-                // Puis on redirige vers l'accueil
                 return $this->redirect($this->generateUrl('sdzblog_accueil'));
             }
         }
         return $this->redirect($this->generateUrl('sdzblog_accueil'));
     }
-
+    
+    /**
+     * Function to add a comment
+     * @param \Sdz\BlogBundle\Entity\Article $article
+     * @return TwigTemplate
+     */
     public function ajouterCommentaireAction(Article $article) {
         $commentaire = new Commentaire;
         $commentaire->setArticle($article);
         $commentaire->setIp($this->getRequest()->server->get('REMOTE_ADDR'));
 
         $form = $this->getCommentaireForm($article, $commentaire);
-
         $request = $this->getRequest();
 
-        // Avec la route que l'on a, nous sommes forcément en POST ici, pas besoin de le retester
+        // route force POST method, test is not required
         $form->bind($request);
         if ($form->isValid()) {
             $em = $this->getDoctrine()->getManager();
             $em->persist($commentaire);
             $em->flush();
             
-            // création de l'ACL
             $aclProvider = $this->get('security.acl.provider');
             $objectIdentity = ObjectIdentity::fromDomainObject($commentaire);
             $acl = $aclProvider->createAcl($objectIdentity);
             
+            // if user is logged allow him to delete his post
             if (null != $this->getUser() ) {
                 $user = $this->getUser();
                 $securityIdentity = UserSecurityIdentity::fromAccount($user);
                 $acl->insertObjectAce($securityIdentity, MaskBuilder::MASK_OWNER);
             }
-            
+            // the admin have is authorised to delete post
             $roleSecurityIdentity = new RoleSecurityIdentity('ROLE_ADMIN');
 	    $acl->insertObjectAce($roleSecurityIdentity, MaskBuilder::MASK_MASTER);
 	    $aclProvider->updateAcl($acl);
 
             return $this->redirect($this->generateUrl('sdzblog_voir', array('slug' => $article->getSlug() ) ) );
         }
-
+        
+        // if the comment is not added -> display error, this function control IP with antifloodvalidator
         $error_message = $this->get('translator')->trans('blog.comment.error');
         $this->get('session')->getFlashBag()->add('danger', $error_message);
 
-
-        // On réaffiche le formulaire sans redirection (sinon on perd les informations du formulaire)
-        // générer la route de l'article et les paramètres
+        // not redirect to keep params
         return $this->forward('SdzBlogBundle:blog:voir', array(
                     'article' => $article,
                     'form' => $form,
@@ -266,16 +281,16 @@ class BlogController extends Controller {
     }
 
     /**
-     * Retourne le formulaire d'ajout d'un commentaire
+     * Function to return the comment form
      * @param Article $article
      * @return Form
      */
     protected function getCommentaireForm(Article $article, Commentaire $commentaire = null) {
+        
         if (null === $commentaire) {
             $commentaire = new Commentaire;
         }
 
-        // Si l'utilisateur courant est identifié, on l'ajoute au commentaire
         if (null !== $this->getUser()) {
             $commentaire->setUser($this->getUser());
         }
@@ -283,38 +298,35 @@ class BlogController extends Controller {
         return $this->createForm(new CommentaireType(), $commentaire);
     }
 
-    
+    /**
+     * Function to delete a comment
+     * @param \Sdz\BlogBundle\Entity\Commentaire $commentaire
+     * @return type
+     * @throws AccessDeniedException
+     */
     public function supprimerCommentaireAction(Commentaire $commentaire) {
         $form = $this->createDeleteForm($commentaire->getId());
         $request = $this->getRequest();
-        
         $securityContext = $this->get('security.context');
         
-        
-        
-        
+        // check if the user have rights
         if (false === $securityContext->isGranted('EDIT', $commentaire) 
             && false === $securityContext->isGranted('ROLE_ADMIN')) {
             throw new AccessDeniedException();
         }
         
         $form->bind($request);
-
-        if ($form->isValid()) { // Ici, isValid ne vérifie donc que le CSRF
-            // On supprime l'article
+        if ($form->isValid()) {
+           
             $em = $this->getDoctrine()->getManager();
             $em->remove($commentaire);
             $em->flush();
 
-            // On définit un message flash
             $this->get('session')->getFlashBag()->add('info', 'Commentaire bien supprimé');
 
             return $this->redirect($this->generateUrl('sdzblog_voir', array('slug' => $commentaire->getArticle()->getSlug())));
         }
     }
-    
-
-    
     
 
 }
