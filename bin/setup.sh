f0f2d53c254325effa62708321fbb7561511cf01
bastieneichenberger <bastien.eichenberger>
2014-11-02 09:21:19 +0100

    add setup.sh

---
+++ bin/setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#update
+git reset --hard origin/master
+git pull https://github.com/bastienEichenberger/extendscript-library-website.git
+
+# Pull symfony components, clear caches and set rights
+# remove the cache prod dir if neccessary
+rm -rf /var/www/extendscript-library-website/app/cache/prod
+
+# install 
+php app/console cache:clear
+php app/console assets:install web --symlink
+php app/console assetic:dump --env=prod
+
+# commands
+php app/console map:update
+php app/console sitemap:generate
+
+# set rights
+chmod -R 777 /var/www/extendscript-library-website/app/cache
+chmod -R 777 /var/www/extendscript-library-website/app/logs
+chown -R www-data: /var/www/extendscript-library-website/web/*
+
+#files data.json and coutries.json have to be edited
+chown -R www-data: /var/www/extendscript-library-website/src/Jlfa/WebSiteBundle/Resources/public/data
