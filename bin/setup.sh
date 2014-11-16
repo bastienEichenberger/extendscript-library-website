7c581590cc44da3f1b66c975f909afa958cbbd85
bastieneichenberger <bastien.eichenberger>
2014-11-16 09:54:42 +0100

    fix bug with ascetic config

--- bin/setup.sh
+++ bin/setup.sh
@@ -11,7 +11,7 @@ rm -rf /var/www/extendscript-library-website/app/cache
 # install 
 php app/console cache:clear --env=prod
 php app/console assets:install web --symlink --env=prod
-php app/console assetic:dump --env=prod
+php app/console assetic:dump --env=prod --no-debug
 
 # commands
 php app/console map:update
